be62e8a593a6fe94d8ad2537cdf0a66b04937118
Tatyana Asenova <tasenova@devrix.com>
2021-11-15 14:57:15 +0200

    Add details page

--- src/App.js
+++ src/App.js
@@ -11,19 +11,31 @@ import ErrorPage from './components/404';
 
 function App() {
 	const [page, setPage] = useState('/home');
-	// For each path, use create Element for each view
-	const routes = {
-		'/home': <WelcomeWorld />,
-		'/games': <GameCatalog />,
-		'/login': <Login />,
-		'/register': <Register />,
-		'/create-game': <CreateGame />,
 
-	}
 	const navigationChangeHandler = (path) => {
 		console.log(path)
 		setPage(path);
 	}
+	// For each path, use create Element for each view
+
+	const router = (path) => {
+		let pathNames = path.split('/');
+		let rootPath = pathNames[1];
+		let argument = pathNames[2];
+
+		const routes = {
+			'home': <WelcomeWorld />,
+			'games': <GameCatalog navigationChangeHandler = {navigationChangeHandler}/>,
+			'login': <Login />,
+			'register': <Register />,
+			'create-game': <CreateGame />,
+			'details': <GameDetails id={argument}/>
+
+		}
+		return routes[rootPath];
+
+	}
+
     return (
         <div id="box">
 			{/* Callbacks from parent to child component will help child trigger change in parent */}
@@ -33,7 +45,7 @@ function App() {
 
             <main id="main-content">
 				{/* All elements passed in between the component start and end will be rendered inside it though props.children */}
-				{ routes[page] || <ErrorPage> <span>Not the druids you are looking for, eh?</span></ErrorPage> }
+				{ router(page) || <ErrorPage> <span>Not the druids you are looking for, eh?</span></ErrorPage> }
             </main>
 
         </div>
--- src/services/gameService.js
+++ src/services/gameService.js
@@ -4,3 +4,8 @@ export function getAll() {
     return fetch(`${baseUrl}/games?sortBy=_createdOn%20desc`)
         .then(res => res.json())
 }
+
+export function getOne(id) {
+	return fetch(`${baseUrl}/games/${id}`)
+		.then(res=> res.json())
+}
