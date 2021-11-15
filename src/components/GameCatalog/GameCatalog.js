9396067d111a3edddf0810f8bbde041a11fb98e1
Tatyana Asenova <tasenova@devrix.com>
2021-11-15 14:24:37 +0200

    Create game service for all requests. Architecture improvement

--- src/components/GameCatalog/GameCatalog.js
+++ src/components/GameCatalog/GameCatalog.js
@@ -1,23 +1,28 @@
 // on component mount -> the fetch request will be sent, hence - useEffect
 import { useEffect, useState } from 'react';
 import GameCard from './GameCard';
+import * as gameService from '../../services/gameService';
 
 const GameCatalog = () => {
 	//Default is an empty array
 	const [games, setGames] = useState([])
 	useEffect(() => {
-		fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-			.then(res=> res.json())
-			.then(result => setGames(result))
+			setTimeout(()=> {
+				// Fetch is performed in the respective service
+				gameService.getAll()
+					.then(result => {
+						setGames(result)
+					})
+			}, 1000)
 	}, [])
 
 	return (
 		<section id="catalog-page">
 		<h1>All Games</h1>
+		{ games.length > 0
+			? games.map(x => <GameCard key={x._id} game={x} />)
+			: <h3 className="no-articles">No games yet</h3> }
 
-		{ games.map(x => <GameCard game={x} />)}
-
-		<h3 className="no-articles">No articles yet</h3>
 	</section>
 	)
 }
